e7fd9e25dbb948a2abbadc2ae461504a24b9a492
Eric Li <15696082812@163.com>
2025-06-28 00:23:38 +0200

    a

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -8,7 +8,11 @@ public class EnemyBase : MonoBehaviour, IDamageable
     public EnemyStats stats;
     protected HealthSystem health;
     public bool canBeDamaged = true;
-    protected bool canAct = true;
+    public bool canAct = true;
+    [SerializeField] protected float coolDown = 1f;
+    protected float timer;
+    public GameObject projectilePrefab;
+    [SerializeField] private Transform shootPoint;
     
     public void TakeDamage(int damageAmount, float stun) {
         if (!canBeDamaged) return;
@@ -26,6 +30,30 @@ public class EnemyBase : MonoBehaviour, IDamageable
 
     private void Start() {
         health = GetComponent<HealthManager>().healthSystem;
+    }
+    private void FixedUpdate()
+    {
+        if (!canAct) return;
+
+        if (timer < coolDown)
+        {
+            timer += Time.fixedDeltaTime;
+        }
+        else
+        {
+            timer = 0;
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        GameObject proj = Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+        AttackComponent bac = proj.AddComponent<AttackComponent>();
+        bac.damageAmount = 1;
+        Rigidbody2D brb = proj.GetComponent<Rigidbody2D>();
+        brb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        ProjectileController bulletCtrl = proj.AddComponent<ProjectileController>();
+        bulletCtrl.Init(Vector2.left, 1f, 5f);
     }
     public virtual void Die() {
         Destroy(gameObject, 0f);
